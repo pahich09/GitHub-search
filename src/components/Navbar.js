5839ed01e13d46824f0b6e60359445eb16cb85d2
Pavel Grebeniuk <grebeniuk.p@gmail.com>
2020-04-10 11:02:46 +0300

    add page components

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="navbar-brand">Github поиск</div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink exact className="nav-link" to="/">Главная </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/profile">Профиль</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">Информация</NavLink>
+          </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = props => {
+  return (
+    <div className="jumbotron">
+      <h2>Информация</h2>
+      <p className="lead">Версия приложения <strong>0.0.1</strong></p>
+    </div>
+  )
+}
+
+export default About
